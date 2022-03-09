6e68608bf82f7a511a570e17a08d23b59a6d17f8
furiopiccinini <furiopiccinini@gmail.com>
2022-03-09 09:43:22 +0100

    FT-32 Doxygen comments
    Modified all comments to support Doxygen

--- classes/interface.py
+++ classes/interface.py
@@ -1,33 +1,43 @@
-# This module loads the inteface and provides the logic.
+##
+# This module loads the interface and provides the logic.
 # The .ui file is imported instead converting it with pyuic5 or PySide.
 # If you modify the .ui file in Qt Designer, there's no need to convert it.
+# @module interface.py - Loads the .ui file and add the logic
 
+# import statements
 from PyQt5 import uic
 from PyQt5.QtWidgets import (QMainWindow, QPushButton,
                              QSlider, QLabel)
 
-# main class for the application
+##
+# @class UI - loads the .ui file made in Qt Designer
+#
 class UI(QMainWindow):
     def __init__(self):
-        # catching global variables from outer scope
+        ##
+        # Defining global variables
+        # @param slider - an array of slider objects
+        # @param label - the label in the interface
         global sliders
         global label
         # parent class init
         super().__init__()
 
-        # this code loads the ui file
+        ##
+        # This code loads the ui file
+        # @method loadUi - loads the .ui file from Qt Designer
         uic.loadUi("testwindow.ui", self)
 
-        # get the button element from UI file
+        # Get the button element from UI file
         button = self.findChild(QPushButton, 'printButton')
-        # get the sliders from UI file
+        # Get the sliders from UI file
         slider_1 = self.findChild(QSlider, 'verticalSlider')
         slider_2 = self.findChild(QSlider, 'verticalSlider_2')
         slider_3 = self.findChild(QSlider, 'verticalSlider_3')
-        # get the label and set it to an empty string
+        # Get the label and set it to an empty string
         label = self.findChild(QLabel, 'label')
         label.setText("Get values...")
-        # initialize sliders to 0
+        # Initialize sliders to 0
         slider_1.setValue(0)
         slider_2.setValue(0)
         slider_3.setValue(0)
@@ -36,7 +46,8 @@ class UI(QMainWindow):
         # connect the clicked event to click_btn method
         button.clicked.connect(self.click_btn)
 
-    ## click_btn method
+    ##
+    # @ method click_btn - loops through the sliders array and intercepts the values
     def click_btn(self):
         # catch global var sliders
         global sliders
@@ -45,6 +56,6 @@ class UI(QMainWindow):
             val = sld.value()
             value += str(val) + " + "
         print("button clicked")
-        # strip the unnecessary 2 chars at the end
+        # Strip the unnecessary 2 chars at the end
         value = value[:-2]
         self.label.setText(value)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-# Test app: import a GUI file made with QT Designer and add some logic.
-# The .ui file is imported instead converting it with pyuic5 or PySide
+##
+# @brief Import a GUI file made with QT Designer and add some logic. The .ui file is imported instead converting it with pyuic5 or PySide
 
 # Import declarations
 from PyQt5.QtWidgets import QApplication
